51abdc936f9c8917a03cca18480611750e6b6c03
fujisaki kengo <abvvl68916@yahoo.co.jp>
2023-09-23 01:53:03 +0900

    add: 利用規約・プライバシーポリシー追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: 'static_pages#top'
+  get 'terms', to: 'static_pages#terms'
+  get 'privacy_policy', to: 'static_pages#privacy_policy'
   devise_for :users
   resource :my_page, only: %i[show edit update]
 end
