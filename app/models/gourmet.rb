8de0797232195519548f7a88f4bbc5454d596ee4
fujisaki kengo <abvvl68916@yahoo.co.jp>
2023-12-11 17:23:29 +0900

    add: ActiveStorage導入

--- app/models/gourmet.rb
+++ app/models/gourmet.rb
@@ -1,4 +1,6 @@
 class Gourmet < ApplicationRecord
+  has_one_attached :image
+
   belongs_to :prefecture
   has_many :shop_gourmets, dependent: :destroy
   has_many :shops, through: :shop_gourmets, dependent: :destroy
