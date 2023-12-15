8ac03397256ec943929f06c28a8e733b287a4afe
fujisaki kengo <abvvl68916@yahoo.co.jp>
2023-12-16 04:15:15 +0900

    add: デフォルトでお気に入りリスト作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  after_create :create_initial_list
+
   has_many :bookmark_lists, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 255 }
+
+  private
+
+  def create_initial_list
+    bookmark_lists.create(name: 'お気に入り')
+  end
 end
