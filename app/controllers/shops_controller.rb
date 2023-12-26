7aa494f1ba584c74cc7e4a773cb54a50a864d395
fujisaki kengo <abvvl68916@yahoo.co.jp>
2023-12-26 18:13:29 +0900

    fix: ショップ情報の取得保存方法を変更

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -5,58 +5,25 @@ class ShopsController < ApplicationController
     gon.latitude = 34.397667
     gon.longitude = 132.4728037
 
-    @client = GooglePlaces::Client.new(ENV['API_KEY'])
-
     if params[:address].present?
       location = Geocoder.search(params[:address]).first
       if location
         gon.latitude = location.latitude
         gon.longitude = location.longitude
         prefecture = get_prefecture_from_coordinates(gon.latitude, gon.longitude)
-        places_data = fetch_shops_by_prefecture(prefecture)
+        @shops = fetch_shops_by_prefecture(prefecture, gon.latitude, gon.longitude)
+        gon.markerData = @shops
       else
-        places_data = []
+        @shops = []
       end
     elsif params[:latitude].present? && params[:longitude].present?
       gon.latitude = params[:latitude]
       gon.longitude = params[:longitude]
       prefecture = get_prefecture_from_coordinates(gon.latitude, gon.longitude)
-      places_data = fetch_shops_by_prefecture(prefecture)
+      @shops = fetch_shops_by_prefecture(prefecture, gon.latitude, gon.longitude)
+      gon.markerData = @shops
     else
-      places_data = []
-    end
-
-    gon.markerData = places_data
-
-    @shops = places_data.map do |place_data|
-      shop = Shop.find_or_initialize_by(place_id: place_data.place_id)
-      unless shop.persisted?
-
-        if place_data.photos.present?
-          photo = place_data.photos.first
-          photo_url = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=#{photo.photo_reference}&key=#{ENV['API_KEY']}"
-        end
-
-        shop.attributes = {
-          name: place_data.name,
-          address: place_data.formatted_address || place_data.vicinity,
-          phone_number: place_data.formatted_phone_number,
-          opening_hours: place_data.opening_hours&.[]("weekday_text")&.join(", ") || "N/A",
-          image: photo_url,
-          website: place_data.website,
-          place_id: place_data.place_id,
-          latitude: place_data.lat,
-          longitude: place_data.lng,
-          rating: place_data.rating,
-          # total_ratings: place_data.user_ratings_total || 0,
-          price_level: place_data.price_level
-        }
-        shop.save
-
-        gourmets = Gourmet.where(name: place_data.keyword)
-        shop.gourmets << gourmets
-      end
-      shop
+      @shops = []
     end
   end
 
@@ -73,123 +40,176 @@ class ShopsController < ApplicationController
   end
 
   # 都道府県に基づいて店舗を取得
-  def fetch_shops_by_prefecture(prefecture)
+  def fetch_shops_by_prefecture(prefecture, latitude, longitude)
     case prefecture
     when '北海道'
-      fetch_shops(['ジンギスカン', 'スープカレー', '札幌ラーメン', '海鮮丼'])
+      fetch_shops(['ジンギスカン', 'スープカレー', '札幌ラーメン', '海鮮丼'], latitude, longitude)
     when '青森県'
-      fetch_shops(['十和田バラ焼き', 'せんべい汁', 'つゆ焼きそば', '大間マグロ'])
+      fetch_shops(['十和田バラ焼き', 'せんべい汁', 'つゆ焼きそば', '大間マグロ'], latitude, longitude)
     when '岩手県'
-      fetch_shops(['盛岡冷麺', 'わんこそば', 'じゃじゃ麺', '前沢牛'])
+      fetch_shops(['盛岡冷麺', 'わんこそば', 'じゃじゃ麺', '前沢牛'], latitude, longitude)
     when '宮城県'
-      fetch_shops(['牛タン', 'ずんだ', '笹かまぼこ', 'はらこ飯'])
+      fetch_shops(['牛タン', 'ずんだ', '笹かまぼこ', 'はらこ飯'], latitude, longitude)
     when '秋田県'
-      fetch_shops(['きりたんぽ', '比内地鶏', '横手焼きそば', 'ハタハタ'])
+      fetch_shops(['きりたんぽ', '比内地鶏', '横手焼きそば', 'ハタハタ'], latitude, longitude)
     when '山形県'
-      fetch_shops(['米沢牛', '芋煮', '玉こんにゃく', '米沢ラーメン'])
+      fetch_shops(['米沢牛', '芋煮', '玉こんにゃく', '米沢ラーメン'], latitude, longitude)
     when '福島県'
-      fetch_shops(['喜多方ラーメン', 'なみえ焼きそば', '円盤餃子', 'ソースカツ丼'])
+      fetch_shops(['喜多方ラーメン', 'なみえ焼きそば', '円盤餃子', 'ソースカツ丼'], latitude, longitude)
     when '茨城県'
-      fetch_shops(['あんこう鍋', '常陸牛', '水戸納豆', '常陸秋そば'])
+      fetch_shops(['あんこう鍋', '常陸牛', '水戸納豆', '常陸秋そば'], latitude, longitude)
     when '栃木県'
-      fetch_shops(['宇都宮餃子', '佐野ラーメン', '宇都宮焼きそば', 'いもフライ'])
+      fetch_shops(['宇都宮餃子', '佐野ラーメン', '宇都宮焼きそば', 'いもフライ'], latitude, longitude)
     when '群馬県'
-      fetch_shops(['ひもかわうどん', '焼きまんじゅう', 'おっきりこみ', '水沢うどん'])
+      fetch_shops(['ひもかわうどん', '焼きまんじゅう', 'おっきりこみ', '水沢うどん'], latitude, longitude)
     when '埼玉県'
-      fetch_shops(['豆腐らーめん', 'すったて', 'くるみそば', '煮ぼうとう'])
+      fetch_shops(['豆腐らーめん', 'すったて', 'くるみそば', '煮ぼうとう'], latitude, longitude)
     when '千葉県'
-      fetch_shops(['なめろう', '勝浦タンタンメン', 'ホワイト餃子', '竹岡ラーメン'])
+      fetch_shops(['なめろう', '勝浦タンタンメン', 'ホワイト餃子', '竹岡ラーメン'], latitude, longitude)
     when '東京都'
-      fetch_shops(['江戸前寿司', '深川めし', 'もんじゃ焼き', 'どぜう鍋'])
+      fetch_shops(['江戸前寿司', '深川めし', 'もんじゃ焼き', 'どぜう鍋'], latitude, longitude)
     when '神奈川県'
-      fetch_shops(['サンマーメン', '横浜家系ラーメン', '牛鍋', 'しらす'])
+      fetch_shops(['サンマーメン', '横浜家系ラーメン', '牛鍋', 'しらす'], latitude, longitude)
     when '新潟県'
-      fetch_shops(['へぎそば', 'イタリアン焼きそば', 'タレかつ丼', 'のどぐろ'])
+      fetch_shops(['へぎそば', 'イタリアン焼きそば', 'タレかつ丼', 'のどぐろ'], latitude, longitude)
     when '富山県'
-      fetch_shops(['富山ブラックラーメン', 'ホタルイカ', 'ます寿司', '氷見うどん'])
+      fetch_shops(['富山ブラックラーメン', 'ホタルイカ', 'ます寿司', '氷見うどん'], latitude, longitude)
     when '石川県'
-      fetch_shops(['能登丼', '金沢カレー', 'ハントンライス', '金沢おでん'])
+      fetch_shops(['能登丼', '金沢カレー', 'ハントンライス', '金沢おでん'], latitude, longitude)
     when '福井県'
-      fetch_shops(['越前がに', '越前そば', 'ソースカツ丼', 'ボルガライス'])
+      fetch_shops(['越前がに', '越前そば', 'ソースカツ丼', 'ボルガライス'], latitude, longitude)
     when '山梨県'
-      fetch_shops(['ほうとう', '吉田うどん', '甲府鳥もつ煮', '甲州ワインビーフ'])
+      fetch_shops(['ほうとう', '吉田うどん', '甲府鳥もつ煮', '甲州ワインビーフ'], latitude, longitude)
     when '長野県'
-      fetch_shops(['信州そば', 'おやき', '信州サーモン', '山賊焼き'])
+      fetch_shops(['信州そば', 'おやき', '信州サーモン', '山賊焼き'], latitude, longitude)
     when '岐阜県'
-      fetch_shops(['飛騨牛', '鶏ちゃん', '高山ラーメン', '朴葉味噌'])
+      fetch_shops(['飛騨牛', '鶏ちゃん', '高山ラーメン', '朴葉味噌'], latitude, longitude)
     when '静岡県'
-      fetch_shops(['静岡おでん', '富士宮やきそば', '浜松餃子', 'うなぎ'])
+      fetch_shops(['静岡おでん', '富士宮やきそば', '浜松餃子', 'うなぎ'], latitude, longitude)
     when '愛知県'
-      fetch_shops(['手羽先', 'ひつまぶし', '味噌カツ', 'きしめん'])
+      fetch_shops(['手羽先', 'ひつまぶし', '味噌カツ', 'きしめん'], latitude, longitude)
     when '三重県'
-      fetch_shops(['伊勢うどん', 'てこね寿司', '松阪牛', '伊勢海老'])
+      fetch_shops(['伊勢うどん', 'てこね寿司', '松阪牛', '伊勢海老'], latitude, longitude)
     when '滋賀県'
-      fetch_shops(['近江牛', '近江ちゃんぽん', 'ふなずし', '鴨鍋'])
+      fetch_shops(['近江牛', '近江ちゃんぽん', 'ふなずし', '鴨鍋'], latitude, longitude)
     when '京都府'
-      fetch_shops(['おばんざい', '湯豆腐', '京都ラーメン', '抹茶スイーツ'])
+      fetch_shops(['おばんざい', '湯豆腐', '京都ラーメン', '抹茶スイーツ'], latitude, longitude)
     when '大阪府'
-      fetch_shops(['たこ焼き', '串カツ', 'お好み焼き', 'てっちり'])
+      fetch_shops(['たこ焼き', '串カツ', 'お好み焼き', 'てっちり'], latitude, longitude)
     when '兵庫県'
-      fetch_shops(['明石焼き', '神戸牛', '姫路おでん', 'そばめし'])
+      fetch_shops(['明石焼き', '神戸牛', '姫路おでん', 'そばめし'], latitude, longitude)
     when '奈良県'
-      fetch_shops(['三輪そうめん', '柿の葉寿司', '天理ラーメン', '飛鳥鍋'])
+      fetch_shops(['三輪そうめん', '柿の葉寿司', '天理ラーメン', '飛鳥鍋'], latitude, longitude)
     when '和歌山県'
-      fetch_shops(['和歌山ラーメン', 'めはり寿司', '生まぐろ', 'クエ鍋'])
+      fetch_shops(['和歌山ラーメン', 'めはり寿司', '生まぐろ', 'クエ鍋'], latitude, longitude)
     when '鳥取県'
-      fetch_shops(['鳥取牛骨ラーメン', 'モサエビ', '松葉がに', 'ホルそば'])
+      fetch_shops(['鳥取牛骨ラーメン', 'モサエビ', '松葉がに', 'ホルそば'], latitude, longitude)
     when '島根県'
-      fetch_shops(['出雲そば', 'のどぐろ', '松江おでん', 'しじみ'])
+      fetch_shops(['出雲そば', 'のどぐろ', '松江おでん', 'しじみ'], latitude, longitude)
     when '岡山県'
-      fetch_shops(['津山ホルモンうどん', 'デミかつ丼', 'ばらずし', 'えびめし'])
+      fetch_shops(['津山ホルモンうどん', 'デミかつ丼', 'ばらずし', 'えびめし'], latitude, longitude)
     when '広島県'
-      fetch_shops(['広島風お好み焼き', '牡蠣', '広島つけ麺', '尾道ラーメン'])
+      fetch_shops(['広島風お好み焼き', '牡蠣', '広島つけ麺', '尾道ラーメン'], latitude, longitude)
     when '山口県'
-      fetch_shops(['ふぐ', '瓦そば', '岩国寿司', 'ばりそば'])
+      fetch_shops(['ふぐ', '瓦そば', '岩国寿司', 'ばりそば'], latitude, longitude)
     when '徳島県'
-      fetch_shops(['徳島ラーメン', '阿波尾鶏', '鳴門鯛', '祖谷そば'])
+      fetch_shops(['徳島ラーメン', '阿波尾鶏', '鳴門鯛', '祖谷そば'], latitude, longitude)
     when '香川県'
-      fetch_shops(['讃岐うどん', '骨付鳥', 'ぴっぴ飯', 'かっしゃ焼'])
+      fetch_shops(['讃岐うどん', '骨付鳥', 'ぴっぴ飯', 'かっしゃ焼'], latitude, longitude)
     when '愛媛県'
-      fetch_shops(['鯛めし', '八幡浜ちゃんぽん', '今治焼き鳥', '焼豚玉子飯'])
+      fetch_shops(['鯛めし', '八幡浜ちゃんぽん', '今治焼き鳥', '焼豚玉子飯'], latitude, longitude)
     when '高知県'
-      fetch_shops(['カツオ', '鍋焼きラーメン', '屋台餃子', '室戸キンメ丼'])
+      fetch_shops(['カツオ', '鍋焼きラーメン', '屋台餃子', '室戸キンメ丼'], latitude, longitude)
     when '福岡県'
-      fetch_shops(['博多ラーメン', 'もつ鍋', 'とり皮', '辛子明太子'])
+      fetch_shops(['博多ラーメン', 'もつ鍋', 'とり皮', '辛子明太子'], latitude, longitude)
     when '佐賀県'
-      fetch_shops(['シシリアンライス', '佐賀牛', '呼子イカ', '竹崎かに'])
+      fetch_shops(['シシリアンライス', '佐賀牛', '呼子イカ', '竹崎かに'], latitude, longitude)
     when '長崎県'
-      fetch_shops(['長崎ちゃんぽん', '皿うどん', 'レモンステーキ', '佐世保バーガー'])
+      fetch_shops(['長崎ちゃんぽん', '皿うどん', 'レモンステーキ', '佐世保バーガー'], latitude, longitude)
     when '熊本県'
-      fetch_shops(['馬肉', '太平燕', '熊本ラーメン', 'あか牛'])
+      fetch_shops(['馬肉', '太平燕', '熊本ラーメン', 'あか牛'], latitude, longitude)
     when '大分県'
-      fetch_shops(['とり天', '地獄蒸し料理', '別府冷麺', 'りゅうきゅう'])
+      fetch_shops(['とり天', '地獄蒸し料理', '別府冷麺', 'りゅうきゅう'], latitude, longitude)
     when '宮崎県'
-      fetch_shops(['チキン南蛮', '辛麺', '地鶏炭火焼き', '宮崎牛'])
+      fetch_shops(['チキン南蛮', '辛麺', '地鶏炭火焼き', '宮崎牛'], latitude, longitude)
     when '鹿児島県'
-      fetch_shops(['つけあげ', 'かごしま黒豚', '鹿児島ラーメン', 'しろくま'])
+      fetch_shops(['つけあげ', 'かごしま黒豚', '鹿児島ラーメン', 'しろくま'], latitude, longitude)
     when '沖縄県'
-      fetch_shops(['沖縄そば', 'ゴーヤチャンプルー', 'タコライス', '海ぶどう'])
+      fetch_shops(['沖縄そば', 'ゴーヤチャンプルー', 'タコライス', '海ぶどう'], latitude, longitude)
     else
       []
     end
   end
 
   # キーワードに基づいて店舗を検索
-  def fetch_shops(keywords)
-    unique_shops = {}
+  def fetch_shops(keywords, latitude, longitude)
+    shops = []
     keywords.each do |keyword|
-      results = @client.spots(gon.latitude, gon.longitude, radius: 1000, types: ['restaurant'], keyword: keyword, language: 'ja', rankby: 'prominence').first(5)
-      results.each do |result|
-        if !unique_shops.key?(result.place_id)
-          result.keyword = []
-          result.keyword << keyword
-          unique_shops[result.place_id] = result
-        else
-          unique_shops[result.place_id].keyword << keyword
-        end
+      base_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?"
+      parameters = {
+        location: "#{latitude},#{longitude}",
+        radius: 1000,
+        types: 'restaurant',
+        keyword: keyword,
+        language: 'ja',
+        key: ENV['API_KEY']
+      }
+
+      url = base_url + parameters.to_query
+      response = Net::HTTP.get(URI(url))
+      results = JSON.parse(response)["results"]
+
+      results.each do |place_data|
+        shop = find_or_create_from_api_data(place_data, keyword)
+        shops << shop unless shops.any? { |s| s.place_id == shop.place_id }
       end
     end
-    unique_shops.values
+    shops
+  end
+
+  def find_or_create_from_api_data(place_data, keyword)
+    shop = Shop.find_or_initialize_by(place_id: place_data['place_id'])
+
+    gourmet = Gourmet.find_or_create_by(name: keyword)
+    shop.gourmets << gourmet unless shop.gourmets.exists?(gourmet.id)
+
+    return shop if shop.persisted?
+
+    details = fetch_place_details(place_data['place_id'])
+
+    if details['photos'].present?
+      photo = details['photos'].first
+      photo_url = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=#{photo['photo_reference']}&key=#{ENV.fetch('API_KEY', nil)}"
+    end
+
+    shop.attributes = {
+      name: details['name'],
+      address: details['formatted_address'].sub(/\A[^ ]+/, ''),
+      phone_number: details['formatted_phone_number'],
+      opening_hours: details['opening_hours'] ? details['opening_hours']['weekday_text'].join("\n") : "N/A",
+      image: photo_url,
+      website: details['website'],
+      place_id: details['place_id'],
+      latitude: details['geometry']['location']['lat'],
+      longitude: details['geometry']['location']['lng'],
+      rating: details['rating'],
+      total_ratings: details['user_ratings_total'] || 0,
+      price_level: details['price_level']
+    }
+    shop.save
+    shop
+  end
+
+  def fetch_place_details(place_id)
+    base_url = "https://maps.googleapis.com/maps/api/place/details/json"
+    parameters = {
+      place_id: place_id,
+      fields: 'name,formatted_address,formatted_phone_number,opening_hours,photos,website,place_id,geometry,rating,user_ratings_total,price_level',
+      key: ENV.fetch('API_KEY', nil),
+      language: 'ja'
+    }
+    response = Net::HTTP.get(URI("#{base_url}?#{parameters.to_query}"))
+    JSON.parse(response)['result']
   end
 
   # 座標から都道府県を取得する
