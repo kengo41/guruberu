9042162fff5379332e15c4f20224026c9fba5ed2
fujisaki kengo <abvvl68916@yahoo.co.jp>
2024-01-04 22:48:33 +0900

    fix: ログイン時ホーム画面へ飛ぶよう修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
 
+  def after_sign_in_path_for(resource)
+    home_path
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
