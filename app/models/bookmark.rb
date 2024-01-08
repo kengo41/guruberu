9262b58bb4e94b6f86b87194c7cde463ae787531
fujisaki kengo <abvvl68916@yahoo.co.jp>
2024-01-09 04:13:49 +0900

    fix: リファクタング

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -4,6 +4,10 @@ class Bookmark < ApplicationRecord
 
   validates :bookmark_list_id, uniqueness: { scope: :shop_id }
 
+  def self.bookmark_list_present?(shop, list)
+    find_by(shop: shop, bookmark_list: list).present?
+  end
+
   def self.user_bookmarked_shop?(user, shop)
     user.present? && find_by(shop: shop, bookmark_list: user.bookmark_lists)
   end
