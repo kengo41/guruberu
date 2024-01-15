e8f44a5533d2d2416d86ac63c579ae60e3b8a9ec
fujisaki kengo <abvvl68916@yahoo.co.jp>
2024-01-15 22:33:28 +0900

    add: フィルター機能実装

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -7,19 +7,19 @@ class ShopsController < ApplicationController
     if params[:address].present?
       location = Geocoder.search(params[:address]).first
       if location
-        latitude = location.latitude
-        longitude = location.longitude
-        set_shops(latitude, longitude)
+        @latitude = location.latitude
+        @longitude = location.longitude
+        set_shops(@latitude, @longitude)
       else
         flash.now[:danger] = "入力が正しくありません"
       end
     elsif params[:latitude].present? && params[:longitude].present?
-      latitude = params[:latitude]
-      longitude = params[:longitude]
-      set_shops(latitude, longitude)
+      @latitude = params[:latitude]
+      @longitude = params[:longitude]
+      set_shops(@latitude, @longitude)
     end
 
-    set_map_data(latitude, longitude, @shops)
+    set_map_data(@latitude, @longitude, @shops)
   end
 
   def show
@@ -32,9 +32,26 @@ class ShopsController < ApplicationController
   def set_shops(latitude, longitude)
     prefecture = fetch_prefecture_by_coordinates(latitude, longitude)
     flash.now[:danger] = "お店が見つかりません" unless prefecture.present?
-    gourmets = fetch_gourmet_by_prefectures(prefecture)
-    @shops = fetch_shop_by_gourmets(gourmets, latitude, longitude) if gourmets
+    @gourmets = fetch_gourmet_by_prefectures(prefecture)
+    @shops = fetch_shop_by_gourmets(@gourmets, latitude, longitude) if @gourmets
     flash.now[:danger] = "お店が見つかりません" unless @shops.present?
+    @shops = filter_shops(@shops)
+  end
+
+  def filter_shops(shops)
+    if params[:rating].present?
+      shops.select! { |shop| shop.rating && shop.rating >= params[:rating].to_f }
+    end
+    if params[:total_ratings].present?
+      shops.select! { |shop| shop.total_ratings && shop.total_ratings >= params[:total_ratings].to_i }
+    end
+    if params[:price_level].present?
+      shops.select! { |shop| shop.price_level && shop.price_level == params[:price_level].to_s }
+    end
+    if params[:gourmet].present?
+      shops.select! { |shop| shop.gourmets.any? { |gourmet| gourmet.name == params[:gourmet] } }
+    end
+    shops
   end
 
   def set_map_data(latitude, longitude, marker_data)
@@ -44,17 +61,8 @@ class ShopsController < ApplicationController
   end
 
   def fetch_prefecture_by_coordinates(latitude, longitude)
-    base_url = "https://maps.googleapis.com/maps/api/geocode/json?"
-    parameters = {
-      latlng: "#{latitude},#{longitude}",
-      language: 'ja',
-      key: ENV['API_KEY']
-    }
-
-    url = base_url + parameters.to_query
-    response = Net::HTTP.get(URI(url))
-    address_components = JSON.parse(response)["results"][0]['address_components']
-    prefecture = address_components.find { |component| component['types'].include?('administrative_area_level_1') }
+    location = Geocoder.search([latitude, longitude], language: :ja).first
+    prefecture = location.address_components.find { |component| component['types'].include?('administrative_area_level_1') }
     prefecture['long_name'] if prefecture
   end
 
@@ -65,6 +73,7 @@ class ShopsController < ApplicationController
 
   def fetch_shop_by_gourmets(gourmets, latitude, longitude)
     shops = []
+
     gourmets.each do |gourmet|
       base_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?"
       parameters = {
@@ -80,14 +89,12 @@ class ShopsController < ApplicationController
       response = Net::HTTP.get(URI(url))
       results = JSON.parse(response)["results"]
 
-      sorted_by_user_ratings = results.sort { |a, b| b['user_ratings_total'].to_i <=> a['user_ratings_total'].to_i }.first(10)
-      top_rated_shops = sorted_by_user_ratings.sort_by { |place_data| -place_data['rating'].to_f }.first(5)
-
-      top_rated_shops.each do |place_data|
+      results.each do |place_data|
         shop = Shop.find_or_create_from_api_data(place_data, gourmet)
         shops << shop unless shops.any? { |s| s.place_id == shop.place_id }
       end
     end
+    shops.sort_by! { |shop| -shop.rating.to_f }
     shops
   end
 end
