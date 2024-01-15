f332186549d8b7e9f1490aebf2843bc17fd62619
fujisaki kengo <abvvl68916@yahoo.co.jp>
2024-01-15 22:43:39 +0900

    fix: その他修正

--- app/javascript/google_maps.js
+++ app/javascript/google_maps.js
@@ -7,7 +7,7 @@ var currentInfoWindow = null;
 
 window.initMap = function() {
   map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 15,
+    zoom: 14,
     center: new google.maps.LatLng(lat, lng),
     fullscreenControl: false,
     mapTypeControl: false,
